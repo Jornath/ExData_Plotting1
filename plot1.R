c278922a0ab1f1d68672be9ebc222abc6cdbc8c9
Jornath <jonathanrubens@gmail.com>
2014-06-04 18:15:59 -0500

    Final Commit - Complete Assignment

--- plot1.R
+++ plot1.R
@@ -1,6 +1,12 @@
 ## plot1.R
 ## Data is in the variable "db"
 db <- read.table("household_power_consumption.txt",  na.strings = c("NA", "?", "Not Available"), sep = ";", header = T)
+db$Date <- as.Date(db$Date, "%d/%m/%Y")
+date1 <- as.Date("02/01/2007", "%m/%d/%Y")
+date2 <- as.Date("02/02/2007", "%m/%d/%Y")
+db <- db[db$Date == date1 | db$Date == date2,]
+db$Date <- paste(db$Date, db$Time)
+db$Date <- strptime(db$Date, "%F %H:%M:%S")
 png(filename = "plot1.png", width = 480, height = 480)
 hist(db$Global_active_power, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -2,6 +2,9 @@
 ## Data is in the variable "db"
 db <- read.table("household_power_consumption.txt",  na.strings = c("NA", "?", "Not Available"), sep = ";", header = T)
 db$Date <- as.Date(db$Date, "%d/%m/%Y")
+date1 <- as.Date("02/01/2007", "%m/%d/%Y")
+date2 <- as.Date("02/02/2007", "%m/%d/%Y")
+db <- db[db$Date == date1 | db$Date == date2,]
 db$Date <- paste(db$Date, db$Time)
 db$Date <- strptime(db$Date, "%F %H:%M:%S")
 png(filename = "plot2.png", width = 480, height = 480)
